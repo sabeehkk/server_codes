920cafe8afd6ea0b733cea7e8a8398632839596d
Monica <monica.b.raju001@gmail.com>
2024-11-11 11:18:01 +0530

    updated

--- index.js
+++ index.js
@@ -2,28 +2,21 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 const cors = require('cors');
-
 dotenv.config();
-
 const app = express();
 const PORT = 3000;
-
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
  
 app.post('/contact', async (req, res) => {
     const { firstName,lastName, email, phoneno, message } = req.body;
-    
-    // Check if the incoming data is captured correctly
-    console.log(req.body, 'Incoming data');
-
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
             user: 'mail@defencehousingsociety.com',
             pass: 'goya wito mhbf awpc', 
-        },
+            },
     });
 
     const mailOptions = {
@@ -43,7 +36,6 @@ app.post('/contact', async (req, res) => {
     };
     try {
         await transporter.sendMail(mailOptions);
-        // res.status(200).send('Message sent successfully!');
         res.status(200).json({ message: 'Message sent successfully!' });
     } catch (error) {
         console.error('Error sending email:', error);
